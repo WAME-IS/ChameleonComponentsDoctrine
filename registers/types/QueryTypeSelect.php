258f3caa6d2ecc4feea4ba09e6a8b707f3d28a58
Linzee <d.gmiterko@gmail.com>
2016-08-28 22:25:02 +0200

    ADD category select query

--- registers/types/QueryTypeSelect.php
+++ registers/types/QueryTypeSelect.php
@@ -3,8 +3,12 @@
 namespace Wame\ChameleonComponentsDoctrine\Registers\Types;
 
 use Kdyby\Doctrine\QueryBuilder;
+use Nette\InvalidArgumentException as InvalidArgumentException2;
+use RecursiveIteratorIterator;
 use Wame\ChameleonComponents\Definition\DataDefinition;
+use Wame\ChameleonComponents\Definition\DataDefinitionTarget;
 use Wame\ChameleonComponents\Definition\DataSpace;
+use Wame\ChameleonComponents\Definition\RecursiveTreeDefinitionIterator;
 use Wame\ChameleonComponentsDoctrine\Registers\RelationsRegister;
 use Wame\Core\Registers\RepositoryRegister;
 use WebLoader\InvalidArgumentException;
@@ -56,6 +60,7 @@ class QueryTypeSelect implements IQueryType
             $qb = $repository->createQueryBuilder();
 
             $this->buildQuery($dataSpace->getDataDefinition(), $qb);
+            $this->addQueryHint($dataSpace, $qb);
             $this->addRelations($dataSpace, $qb);
 
             return $qb;
@@ -90,6 +95,23 @@ class QueryTypeSelect implements IQueryType
             $qb->setMaxResults(1);
         }
     }
+    
+    /**
+     * @param DataSpace $dataSpace
+     * @param QueryBuilder $qb
+     */
+    public function addQueryHint($dataSpace, $qb) {
+        $query = $dataSpace->getDataDefinition()->getHint('query');
+        if($query) {
+            if(is_array($query)) {
+                foreach ($query as $q) {
+                    call_user_func($q, $qb);
+                }
+            } else {
+                call_user_func($query, $qb);
+            }
+        }
+    }
 
     /**
      * @param DataSpace $dataSpace
@@ -99,56 +121,63 @@ class QueryTypeSelect implements IQueryType
     {
         $relationHint = $dataSpace->getDataDefinition()->getHint('relation');
         $this->addRelationFromHint($relationHint, $dataSpace, $qb);
-        
+
         $parent = $dataSpace;
         while ($parent = $parent->getParent()) {
-            
+
             $to = $parent->getDataDefinition()->getTarget();
-            
-            if(is_array($relationHint)) {
+
+            if (is_array($relationHint)) {
                 $relationHint = isset($relationHint[$to->getType()]) ? $relationHint[$to->getType()] : null;
             }
-            
-            if($relationHint && !is_string($relationHint)) {
+
+            if ($relationHint && !is_string($relationHint)) {
                 continue;
             }
-            
+
             $relation = $this->relationsRegister->getByTarget($dataSpace->getDataDefinition()->getTarget(), $to, $relationHint);
-            
-            if($relation) {
+
+            if ($relation) {
                 $relation->apply($qb, $dataSpace, $parent);
             }
         }
     }
-    
+
     /**
      * @param DataSpace $dataSpace
      * @param QueryBuilder $qb
      */
-    private function addRelationFromHint($hint, $dataSpace, $qb) {
-        
-        if(is_array($hint)) {
-            foreach($hint as $h) {
+    private function addRelationFromHint($hint, $dataSpace, $qb)
+    {
+
+        if (is_array($hint)) {
+            foreach ($hint as $h) {
                 $this->addRelationFromHint($h, $dataSpace, $qb);
             }
             return;
         }
-        
-        if($hint instanceof IRelation) {
-            $hint->apply($qb, $dataSpace, $this->getChildDataSpaceByType($dataSpace, $hint->getTo()));
+
+        if ($hint instanceof IRelation) {
+            $toDataSpace = $this->getChildDataSpaceByType($dataSpace, $hint->getTo());
+            if ($toDataSpace) {
+                $e = new InvalidArgumentException2("No DataSpace for hinted relation found.");
+                $e->hint = $hint;
+                throw $e;
+            }
+            $hint->apply($qb, $dataSpace, $toDataSpace);
         }
     }
-    
+
     /**
      * @param DataSpace $dataSpace
-     * @param \Wame\ChameleonComponents\Definition\DataDefinitionTarget $target
+     * @param DataDefinitionTarget $target
      */
     private function getChildDataSpaceByType($dataSpace, $target)
     {
         $iterator = new RecursiveIteratorIterator(new RecursiveTreeDefinitionIterator([$dataSpace]), RecursiveIteratorIterator::SELF_FIRST);
-        
-        foreach($iterator as $dataSpace) {
-            if($dataSpace->getDataDefinition()->getTarget() == $target) {
+
+        foreach ($iterator as $dataSpace) {
+            if ($dataSpace->getDataDefinition()->getTarget() == $target) {
                 return $dataSpace;
             }
         }
